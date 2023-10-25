9f7d96bb52342faa8f6f08a5985fa9a1d9c3e51b
Pavel Markitantov <markitantow@gmail.com>
2023-10-25 23:29:52 +0200

    Initial Commit

---
+++ Kantor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kantor
+//
+//  Created by Pavel Markitantov on 25/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
